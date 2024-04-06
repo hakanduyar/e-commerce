37b4455eedce474fdbf94a073061d8da29a0d031
rikords <iamhakanduyar@gmail.com>
2024-04-06 18:59:20 +0300

    "Update .eslintrc.js: add Prettier end-of-line rule"
    Explanation:
    
    - The commit adds the `prettier` plugin to the ESLint configuration file.
    - It also adds a new rule for Prettier, specifying the end-of-line state as 'auto'.
    - This change will ensure consistent line endings according to Prettier's configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
+const { error } = require('console')
+const { EndOfLineState } = require('typescript')
+
 module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', { EndOfLine: 'auto' }],
+  },
 }
